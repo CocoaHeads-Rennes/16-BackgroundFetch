ea21cd65b1c0f49f79c6df39c3f0b398dc60dc63
David Bonnet <dbonnet@regionsjob.com>
2014-02-04 19:04:15 +0100

    Init empty project

---
+++ BackgroundFetch/BackgroundFetch/CHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CHAppDelegate.h
+//  BackgroundFetch
+//
+//  Created by David Bonnet on 04/02/2014.
+//  Copyright (c) 2014 David Bonnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BackgroundFetch/BackgroundFetch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BackgroundFetch
+//
+//  Created by David Bonnet on 04/02/2014.
+//  Copyright (c) 2014 David Bonnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CHAppDelegate class]));
+    }
+}
